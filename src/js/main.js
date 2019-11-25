9dd1bf6471292e6bc8201b943586f6f358c7091d
AnnaDubininaV <anna.dybinina.1@gmail.com>
2019-11-25 04:17:34 +0200

    Brandy

---
+++ src/js/main.js
@@ -0,0 +1,89 @@
+var swiper = new Swiper('.swiper-container1', {
+    spaceBetween: 30,
+    effect: 'fade',
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    loop: true,
+
+});
+
+var swiper2 = new Swiper('.swiper-container2', {
+    slidesPerView: 1,
+    spaceBetween: 45,
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination2',
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 2,
+            centeredSlides: false,
+        },
+        960: {
+            slidesPerView: 3,
+
+        }
+
+    }
+
+});
+
+var swiper3 = new Swiper('.swiper-container3', {
+    slidesPerView: 1,
+    slidesPerColumn: 1,
+    slidesPerColumnFill: 'row',
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
+    breakpoints: {
+        540: {
+            slidesPerView: 2,
+            slidesPerColumn: 1,
+        },
+        720: {
+            slidesPerView: 2,
+            slidesPerColumn: 2,
+        },
+        960: {
+            slidesPerView: 3,
+            slidesPerColumn: 2,
+        },
+        1200: {
+            slidesPerView: 4,
+            slidesPerColumn: 2,
+        }
+    }
+});
+
+var swiper4 = new Swiper('.swiper-container4', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination4',
+        clickable: true,
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 2,
+
+        },
+        720: {
+            slidesPerView: 3,
+
+        },
+
+        1200: {
+            slidesPerView: 4,
+
+        },
+    }
+});
